6bf17dc83317b4eafcb43a2c1f6cbbbd4b42c9b9
Dario <daartv@gmail.com>
2019-12-22 17:35:17 +0100

    Functionality is done, time for some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
+import searchIcon from "./magnifying-glass.svg";
+import "./App.css";
 
 const GET_SEARCH_RESULTS = gql`
-  query getSearchResults {
-    search(query: "amsterdam", first: 2, entities: [CITY, ARTWORK]) {
+  query getSearchResults($searchTerm: String!, $searchFilters: [SearchEntity]) {
+    search(query: $searchTerm, first: 2, entities: $searchFilters) {
       edges {
         node {
           displayLabel
@@ -14,24 +16,85 @@ const GET_SEARCH_RESULTS = gql`
   }
 `;
 
-const App = () => (
-  <div>
-    <h1>
-      Good luck{" "}
-      <span aria-label="Thumbs up" role="img">
-        👍
-      </span>
-    </h1>
-    <pre style={{ whiteSpace: "pre-wrap" }}>
-      <Query query={GET_SEARCH_RESULTS}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-          return JSON.stringify(data);
-        }}
-      </Query>
-    </pre>
-  </div>
-);
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchFilters, setSearchFilters] = useState([]);
+  return (
+    <div className="root">
+      <div className="searchBar">
+        <img height="20px" width="20" src={searchIcon} />
+        <input
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          size="100"
+        />
+      </div>
+      <div className="filters">
+        <div
+          onClick={() =>
+            setSearchFilters([...new Set([...searchFilters, "ARTIST"])])
+          }
+          className={`filter ${
+            searchFilters.includes("ARTIST") ? "selected" : ""
+          }`}
+        >
+          Artists
+        </div>
+        <div
+          onClick={() =>
+            setSearchFilters([...new Set([...searchFilters, "ARTWORK"])])
+          }
+          className={`filter ${
+            searchFilters.includes("ARTWORK") ? "selected" : ""
+          }`}
+        >
+          Artworks
+        </div>
+        <div
+          onClick={() =>
+            setSearchFilters([...new Set([...searchFilters, "ARTICLE"])])
+          }
+          className={`filter ${
+            searchFilters.includes("ARTICLE") ? "selected" : ""
+          }`}
+        >
+          Articles
+        </div>
+        <div
+          onClick={() =>
+            setSearchFilters([...new Set([...searchFilters, "CITY"])])
+          }
+          className={`filter ${
+            searchFilters.includes("CITY") ? "selected" : ""
+          }`}
+        >
+          Cities
+        </div>
+        <div
+          onClick={() =>
+            setSearchFilters([...new Set([...searchFilters, "COLLECTION"])])
+          }
+          className={`filter ${
+            searchFilters.includes("COLLECTION") ? "selected" : ""
+          }`}
+        >
+          Collections
+        </div>
+      </div>
+      <pre style={{ whiteSpace: "pre-wrap" }}>
+        <Query query={GET_SEARCH_RESULTS} variables={{ searchTerm, searchFilters }}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+            console.log(data);
+            return data.search.edges.map(edge => (
+              <div>{edge.node.displayLabel}</div>
+            ));
+          }}
+        </Query>
+      </pre>
+    </div>
+  );
+};
 
 export default App;
